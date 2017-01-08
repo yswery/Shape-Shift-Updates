fb92fdb9784ba3f4e55a710fac3656cbb3388f7d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-08 23:36:17 +1300

    Added values in NZD and USD

--- src/Shape.php
+++ src/Shape.php
@@ -12,43 +12,96 @@ class Shape
     private $rate;
     private $limit;
     private $fee;
+    private $BTCValueUSD;
+    private $ETHValueUSD;
+    private $BTCValueNZD;
+    private $ETHValueNZD;
+
+    function __construct()
+    {
+        $this->getCoinInfo();
+        $this->BTCValueUSD = $this->getBTCValueUSD();
+        $this->BTCValueNZD = $this->getBTCValueNZD();
+        $this->ETHValueUSD = $this->getETHValueUSD();
+        $this->ETHValueNZD = $this->getETHValueNZD();
+
+    }
 
     private function getCoinInfo()
     {
         $uri = "https://shapeshift.io/marketinfo/btc_eth";
 
-        $this->coinInfo = json_decode(file_get_contents($uri), true); // converts it to an array object
+        $result = json_decode(file_get_contents($uri), true); // converts it to an array object
 
-        $this->rate = $this->coinInfo['rate'];
-        $this->limit = $this->coinInfo['limit'];
-        $this->fee = $this->coinInfo['minerFee'];
+        (isset($result['rate'])) ? $this->rate = $result['rate'] : "";
+        (isset($result['limit'])) ? $this->limit = $result['limit'] : "";
+        (isset($result['minerFee'])) ? $this->fee = $result['minerFee'] : "";
+
     }
+
+    private function getBTCValueUSD()
+    {
+        $uri = "https://api.coinbase.com/v2/prices/BTC-USD/buy";
+
+        $btc = json_decode(file_get_contents($uri), true);
+
+        if (isset($btc['data']['amount'])) {
+            return $btc['data']['amount'];
+        }
+        return "error";
+
+    }
+
+    private function getBTCValueNZD()
+    {
+        $uri = "https://api.coinbase.com/v2/prices/BTC-NZD/buy";
+
+        $btc = json_decode(file_get_contents($uri), true);
+
+        if (isset($btc['data']['amount'])) {
+            return $btc['data']['amount'];
+        }
+        return "error";
+    }
+
+    private function getETHValueUSD()
+    {
+        $uri = "https://api.coinbase.com/v2/prices/ETH-USD/buy";
+
+        $eth = json_decode(file_get_contents($uri), true);
+
+        if (isset($eth['data']['amount'])) {
+            return $eth['data']['amount'];
+        }
+        return "error";
+    }
+
+    private function getETHValueNZD()
+    {
+        $uri = "https://api.coinbase.com/v2/prices/ETH-NZD/buy";
+
+        $eth = json_decode(file_get_contents($uri), true);
+
+        if (isset($eth['data']['amount'])) {
+            return $eth['data']['amount'];
+        }
+        return "error";
+    }
+
+
 
     public function sendEmail()
     {
+
         $info = new Details();
-
-        $this->getCoinInfo();
-
-
-        //var_dump($this->coinInfo); die();
-
-
-
         $mail = new PHPMailer();
 
         $mail->SMTPDebug = 3;
+
         //Set PHPMailer to use SMTP.
         $mail->isSMTP();
 
         $mail->Host = "smtp.gmail.com";
-
-        //Set this to true if SMTP host requires authentication to send email
-        $mail->SMTPAuth = true;
-
-        //Provide username and password
-        $mail->Username = $info->email;
-        $mail->Password = $info->pass;
 
         // Gmail SMTP requires TLS encryption
         $mail->SMTPSecure = "tls";
@@ -56,20 +109,36 @@ class Shape
         // TCP Port
         $mail->Port = 587;
 
-        $mail->FromName = "ShapeShift Bitcoin/Ether Update";
+        //Set this to true as Gmail requires authentication to send email
+        $mail->SMTPAuth = true;
+
+        //Provide username and password
+        $mail->Username = $info->email;
+        $mail->Password = $info->pass;
+
+        $mail->FromName = "SS BTC/ETH Update";
 
         $mail->addAddress($info->emailTo);
 
         $mail->isHTML(true);
 
         $mail->Subject = "BTC_ETH Update";
-        $mail->Body = "<b>Bitcoin / Ether Update</b>
+        $mail->Body = "<h4>Shape Shift : Bitcoin / Ether Update</h4>
                        <br>
                        <i>Rate $this->rate</i>
                        <br>
                        <i>Limit $this->limit</i>
                        <br>
                        <i>Fee $this->fee</i>
+                       <br>
+                       <h4>Coinbase : Price update</h4>
+                       <i>Bitcoin USD $$this->BTCValueUSD</i>
+                       <br>
+                       <i>Ether USD $$this->ETHValueUSD</i>
+                       <br><br>
+                       <i>Bitcoin NZD $$this->BTCValueNZD</i>
+                       <br>
+                       <i>Ether NZD $$this->ETHValueNZD</i>
         ";
 
         // Sends the email
