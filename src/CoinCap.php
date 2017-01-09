13676399814ea965477c6c20e9049fb7ac1c183e
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-09 14:01:22 +1300

    Add Coincap data (as used by Shapeshift)

---
+++ src/CoinCap.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SSPro;
+
+
+use PHPMailer;
+
+
+/**
+ * Class CoinCap
+ *  - Gets coin prices from Coincap.io API
+ *
+ * @package SSPro
+ */
+class CoinCap
+{
+
+    /**
+        Sample Returned Data
+
+        [btcPrice] => 895.4
+        [btcCap] => 14407814245
+        [altCap] => 2432225487
+        [dom] => 86
+        [bitnodesCount] => 5658
+     */
+
+    public function getCoinInfo()
+    {
+        $uri = "http://socket.coincap.io/global";
+
+        $result = json_decode(file_get_contents($uri), true); // converts it to an array object
+
+        if (isset($result['btcPrice'])) {
+            return $result['btcPrice'];
+        }
+        return "error";
+
+    }
+
+
+//[btcPrice] => 895.4
+//[btcCap] => 14407814245
+//[altCap] => 2432225487
+//[dom] => 86
+//[bitnodesCount] => 5658
+
+}
+
+
