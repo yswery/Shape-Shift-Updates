76169c3280143e80def0116d83456192eeadefbf
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-13 23:23:24 +1300

    Simple calls to objects

--- Run.php
+++ Run.php
@@ -5,18 +5,34 @@ namespace SSPro;
 require 'vendor/autoload.php';
 
 use Dotenv;
-
 // Loads the .env file with our credentials
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$shape = new ShapeShifter();
-$rate = $shape->getCurrentRate();
 
-if ($rate > 88) {
-    $process = new Process();
-    $status = $process->setTimeEmailSent();
-    if ($status) {
-        $process->sendEmail();
-    } 
-}
+$test = new poloniex();
+$result = $test->get_total_btc_balance();
+var_dump($result);
+die();
+
+
+
+$poloniex = new Poloniex();
+$coindesk = new CoinDesk();
+$coinbase = new Coinbase();
+$coincap = new CoinCap();
+$shapeshifter = new ShapeShifter();
+
+$shapeshifter->set_BTC_ETH();
+$shapeshifter->set_ETH_BTC();
+
+
+
+$poloniex->set_data("ETH");
+$poloniex->set_data("ETC");
+$poloniex->set_data("XMR");
+$poloniex->set_data("AMP");
+$poloniex->set_data("DASH");
+
+
+
