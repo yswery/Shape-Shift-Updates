ae97ed83b09d546cc48cdac547d6c6f119f2f9a2
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-14 23:14:26 +1300

    Call primary methods for data

--- Run.php
+++ Run.php
@@ -10,29 +10,13 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 
-$test = new poloniex();
-$result = $test->get_total_btc_balance();
-var_dump($result);
+$process = new Process();
+
+$process->set_poloniex_Coin_Balances();
+$process->set_Prices_BTC();
+$process->set_ShapeShifter_Rates();
+
 die();
-
-
-
-$poloniex = new Poloniex();
-$coindesk = new CoinDesk();
-$coinbase = new Coinbase();
-$coincap = new CoinCap();
-$shapeshifter = new ShapeShifter();
-
-$shapeshifter->set_BTC_ETH();
-$shapeshifter->set_ETH_BTC();
-
-
-
-$poloniex->set_data("ETH");
-$poloniex->set_data("ETC");
-$poloniex->set_data("XMR");
-$poloniex->set_data("AMP");
-$poloniex->set_data("DASH");
 
 
 
