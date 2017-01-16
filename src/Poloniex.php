a3156f0de85edc7fab9f4971e1c5f8a2aa673cee
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-16 22:23:46 +1300

    Add method to set the reverse values
    Allows the user to see how many coins will be bought for 1 btc

--- src/Poloniex.php
+++ src/Poloniex.php
@@ -6,9 +6,7 @@ class Poloniex
 {
     /**
      * String must have a valid coin to coin reference
-     *
-     * EG
-     *
+     * Example
      * 'BTC_ETH'
      * 'USDT_BTC'
      */
@@ -29,6 +27,27 @@ class Poloniex
         $data = $this->get_data($coin);
 
         $db->insertPriceDB("BTC", $data['last'], 1, "Poloniex");
-        $db->insertPoloniexRateDB($coin, $data['last'], $data['high24hr'], $data['low24hr'], $data['lowestAsk'], $data['highestBid']);
+        $db->insertPoloniexRateDB($coin, round($data['last'],4) , round($data['high24hr'],4), round($data['low24hr'],4));
+    }
+
+
+    /**
+     * Converts the BTC value into the number of coins you will get in return
+     */
+    public function set_data_reverse($coin)
+    {
+        $db = new DBController();
+        $data = $this->get_data($coin);
+        $data['last'] = 1 / $data['last'];
+        $data['high24hr'] = 1 / $data['high24hr'];
+        $data['low24hr'] = 1 / $data['low24hr'];
+        $data['highestBid'] = 1 / $data['highestBid'];
+        $split = explode('_', $coin);
+        $coin = $split[1] . "_" . $split[0];
+
+        /**
+         * Params low/high switched to reflect diff math
+         */
+        $db->insertPoloniexRateDB($coin, round($data['last'],2), round($data['low24hr'],2), round($data['high24hr'],2));
     }
 }
