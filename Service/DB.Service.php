8553c06dfe24a41d0e679cf1403b7152e0c7f188
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-17 23:14:48 +1300

    Add DB Connect

---
+++ Service/DB.Service.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SSPro\Service;
+
+use PDO;
+use PDOException;
+
+class DB extends PDO
+{
+    protected static $instance;
+
+    public function __construct()
+    {
+        try {
+            parent::__construct(getenv('MYSQL_DB'), getenv('MYSQLDB_USER'), getenv('MYSQLDB_PASS'));
+        } catch (PDOException $Exception) {
+            var_dump($Exception); die();
+        }
+    }
+
+
+    public function query($statement, $mode = PDO::ATTR_DEFAULT_FETCH_MODE, $arg3 = null)
+    {
+        parent::query($statement);
+    }
+
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new DB();
+        }
+        return self::$instance;
+    }
+
+}
+
