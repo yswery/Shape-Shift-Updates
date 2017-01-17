01de3961724b091469fd50b63f1b983b8f016b5f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-17 23:15:07 +1300

    Refactor

--- src/ShapeShifter.php
+++ src/ShapeShifter.php
@@ -6,6 +6,7 @@ class ShapeShifter
 {
     private $rate;
 
+
     public function get_BTC_ETH()
     {
         $uri = "https://shapeshift.io/marketinfo/btc_eth";
@@ -18,24 +19,6 @@ class ShapeShifter
         $uri = "https://shapeshift.io/marketinfo/eth_btc";
         $result = json_decode(file_get_contents($uri), true); // converts it to an array object
         return $result;
-    }
-
-    public function set_BTC_ETH()
-    {
-        $db = new DBController();
-        $data = $this->get_BTC_ETH();
-
-        $db->insertRateDB(strtoupper($data['pair']), $data['rate'], $data['limit'], $data['minerFee']);
-
-    }
-
-    public function set_ETH_BTC()
-    {
-        $db = new DBController();
-        $data = $this->get_ETH_BTC();
-
-        $db->insertRateDB(strtoupper($data['pair']), $data['rate'], $data['limit'], $data['minerFee']);
-
     }
 
 
