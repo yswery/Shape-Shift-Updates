e00eeeea00940345174efff07ce990cc00e0a842
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-18 15:38:56 +1300

    Display headings and split into tables

--- Views/home.view.php
+++ Views/home.view.php
@@ -1,4 +1,7 @@
+<?php
 
+    foreach ($viewData as $key => $value) { ?>
+        <legend><?= $key ?></legend>
 <div class="table">
     <table class="table table-bordered table-hover">
         <thead>
@@ -12,15 +15,16 @@
         </thead>
         <tbody>
         <?php
-        foreach ($viewData as $data) { ?>
+        foreach ($value as $d) { ?>
             <tr>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['coin'] ?></td>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['rate_btc'] ?></td>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['limit'] ?></td>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['fee'] ?></td>
-                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $data['created_at'] ?></td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $d['coin'] ?></td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $d['rate_btc'] ?></td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $d['limit'] ?></td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $d['fee'] ?></td>
+                <td class="col-md-1 col-sm-1" style="text-align: center"> <?= $d['created_at'] ?></td>
             </tr>
         <?php } ?>
         </tbody>
     </table>
 </div>
+<?php } ?>
